3633966d6acf7b703e7834153e2c95720ea5f9e7
yuto <s19005@std.it-college.ac.jp>
2019-06-05 10:51:04 +0900

    6/4課題

--- 0604/060403.sh
+++ 0604/060403.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ -d "$1" ]; then
-        for file in $(ls "$1")
+        for file in $(find "$1" -maxdepth 1 -type f)
         do
-		if [ -x "${1}/${file}" ]; then
-			echo "$file"
+		if [ -x "$file" ] ; then
+			echo $(basename "$file")
 		fi
         done
 else
